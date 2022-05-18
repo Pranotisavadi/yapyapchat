baad5abb6ad63ead7d1a8cc35f47ecfaa89150e6
Moises De Leon <mdeleon5826@gmail.com>
2022-05-18 14:56:46 -0400

    added socket.io to front and backend

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const cors = require("cors");
 const authRoute = require("./app/routes/auth");
 
 app.use(express.json());
@@ -7,6 +9,15 @@ app.use(express.json());
 // app.listen("8000", ()=> {
 //     console.log("Backend is running")
 // })
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  } 
+});
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
   const db = require("./app/models");
@@ -26,7 +37,29 @@ db.mongoose
 
   app.use("/api/auth", authRoute);
 
+  app.get("/", (req, res) => {
+    res.send('Server is running.')
+  })
+
+  io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+      socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+      io.to(userToCall).emit("callUser", { signal: signalData, from, name})
+    });
+
+    socket.on("answerCall", (data) => {
+      io.to(data.toString()).emit("callAccepted", data.signal)
+    });
+  })
+
   const PORT = process.env.PORT || 8000;
-  app.listen(PORT, () => {
+
+  server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
   });
+
