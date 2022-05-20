8b498dbd0c52ceebe716734526e71599b88f00ad
Moises De Leon <mdeleon5826@gmail.com>
2022-05-19 21:55:31 -0400

    created routing for app pages. all routes work accordingly

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from './SocketContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
--- client/src/pages/HomeChat.js
+++ client/src/pages/HomeChat.js
@@ -1,20 +1,19 @@
 import '../App.css';
 import LeftBar from '../components/leftbar/LeftBar';
-import Navbar from '../components/navbar/Navbar';
 import { createContext, useState } from "react";
 import ReactSwitch from "react-switch";
 
-const ThemeContext = createContext(null);
+export const ThemeContext = createContext(null);
 
 function HomeChat() {
     const [theme, setTheme] = useState("dark");
     const toggleTheme = () => {
-        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));  
+    }
 
-        return (
+    return (
             <ThemeContext.Provider>
                 <div id={theme}>
-                    <Navbar />
                     <div className='switch'>
                         <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
                         <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
@@ -25,7 +24,6 @@ function HomeChat() {
                 </div>
             </ThemeContext.Provider>
         )
-    }
 }
 
-export default { ThemeContext, HomeChat }
+export default HomeChat;
