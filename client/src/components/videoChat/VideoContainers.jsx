7b4f4c06a8fe8d32249cbb796800b2ac8327b1be
Moises De Leon <mdeleon5826@gmail.com>
2022-05-31 19:06:38 -0400

    videochat is currently working. all files are currently saved under the videochat folder

---
+++ client/src/components/videoChat/VideoContainers.jsx
@@ -0,0 +1,59 @@
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper } from '@mui/material';
+import { SocketContext } from '../../SocketContext';
+import './videoContainer.css';
+// import { VideoCameraFrontOutlined, MicOutlined, IosShareOutlined, CallEndRounded } from "@mui/icons-material";
+
+function VideoContainers() {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
+
+  return (
+    // <div className="video-nav-container">
+      <Grid container className="grid-container">
+        {stream && (
+          <Paper className="paper">
+            <Grid item xs={12} md={6}>
+              <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
+              <video playsInline muted ref={myVideo} autoPlay className="video" />
+            </Grid>
+          </Paper>
+        )}
+        {callAccepted && !callEnded && (
+          <Paper className="paper">
+            <Grid item xs={12} md={6}>
+              <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
+              <video playsInline ref={userVideo} autoPlay className="video" />
+            </Grid>
+          </Paper>
+        )}
+      </Grid>
+    //   <div className="right-side-panel">
+    //     <div className="topSide">
+    //       <div className="time-lapse">
+
+    //       </div>
+    //     </div>
+
+    //     <div className="bottomSide">
+    //       <div className="right-icons-container">
+    //         <div className="video-nav-item">
+    //           <VideoCameraFrontOutlined />
+    //         </div>
+    //         <div className="video-nav-item">
+    //           <MicOutlined />
+    //         </div>
+    //         <div className="video-nav-item">
+    //           <IosShareOutlined />
+    //         </div>
+    //         <button className="end-call-btn" type="button btn-danger">
+    //           <CallEndRounded />
+    //         </button>
+    //       </div>
+    //     </div>
+        
+    //   </div>
+    // </div>
+  )
+}
+
+export default VideoContainers;
--- client/src/pages/VideoChat.js
+++ client/src/pages/VideoChat.js
@@ -1,65 +1,17 @@
-import React, { useContext } from 'react';
-import { Grid, Typography, Paper } from '@mui/material';
-import { SocketContext } from '../SocketContext';
-import '../components/videoChat/videoNav.css';
-import { VideoCameraFrontOutlined, MicOutlined, IosShareOutlined, CallEndRounded } from "@mui/icons-material";
+import React from 'react';
+import '../components/videoChat/videoContainer.css';
+import VideoContainers from '../components/videoChat/VideoContainers';
+import VideoActions from '../components/videoChat/VideoActions';
+import Calls from '../components/videoChat/Calls';
 
 function VideoChat() {
-  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
 
   return (
-    <div className="video-nav-container">
-      <Grid container className="grid-container">
-        {stream && (
-          <Paper className="paper">
-            <Grid item xs={12} md={6}>
-              <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
-              <video playsInline muted ref={myVideo} autoPlay className="video" />
-            </Grid>
-          </Paper>
-        )}
-        {callAccepted && !callEnded && (
-          <Paper className="paper">
-            <Grid item xs={12} md={6}>
-              <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
-              <video playsInline ref={userVideo} autoPlay className="video" />
-            </Grid>
-          </Paper>
-        )}
-      </Grid>
-      <div className="right-side-panel">
-        <div className="topSide">
-          <div className="time-lapse">
-
-          </div>
-        </div>
-
-        <div className="bottomSide">
-          <div className="right-icons-container">
-            <div className="video-nav-item">
-              <VideoCameraFrontOutlined />
-            </div>
-            <br/>
-            <div className="video-nav-item">
-              <MicOutlined />
-            </div>
-            <br/>
-            <div className="video-nav-item">
-              <IosShareOutlined />
-            </div>
-            <br/>
-            {/* <button className="end-call-btn" type="button btn-danger">
-              <CallEndRounded />
-            </button> */}
-
-            <div className="video-nav-item">
-            <CallEndRounded />
-            </div>
-            
-          </div>
-        </div>
-
-      </div>
+    <div className="video-chat-wrapper">
+      <VideoContainers />
+      <VideoActions>
+        <Calls/>
+      </VideoActions>
     </div>
 
   )
